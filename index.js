3edf0ca1a824af5b1744e247caafa8f135deab0a
s.sriramprasath@gmail.com <s.sriramprasath@gmail.com>
2018-10-18 21:36:58 +0530

    initialcommit

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var databaseconnection = require('./Dbconnection/config.js');
+console.log("Initializing...");
+mongoose.Promise = global.Promise;
+
+function check(){
+mongoose.connect(databaseconnection.url,{useNewUrlParser:true}).then(()=>{
+    console.log("****** Mongodb Connected @ Cloud ******");
+     clearInterval(clearint);
+}).catch(err =>{
+    console.log("XXXX Mongodb !Connected  @ Cloud XXXX");
+    clearInterval(clearint);
+});
+}
+
+var clearint = setInterval(check,3000);
+
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.json());
+app.use(cors());
+require('./Routes/routes')(app);
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log(port);
